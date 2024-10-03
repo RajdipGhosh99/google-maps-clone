aa84ed45fd72a0ad70df36e7d2ffd30f890dfe8e
Rajdip Ghosh <raazdeepghosh@gmail.com>
2024-10-03 12:09:31 +0530

    updated

--- src/app/maps/maps.component.ts
+++ src/app/maps/maps.component.ts
@@ -6,11 +6,17 @@ import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-direct
 import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css';
 import { MapboxStyleSwitcherControl } from "mapbox-gl-style-switcher";
 import "mapbox-gl-style-switcher/styles.css"
+import MapboxDraw from '@mapbox/mapbox-gl-draw';
+import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css';
+import * as turf from '@turf/turf';
+import { MapEventType } from 'mapbox-gl';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-maps',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './maps.component.html',
   styleUrl: './maps.component.scss'
 })
@@ -22,6 +28,8 @@ export class MapsComponent {
   directions: any;
   markers: mapboxgl.Marker[] = []
   currentLocation!: mapboxgl.Marker
+  mapMode: 1 | 2 = 1
+  draw: typeof MapboxDraw
 
   constructor() { }
 
@@ -33,7 +41,7 @@ export class MapsComponent {
     // Add map controls
     this.map.addControl(new mapboxgl.NavigationControl());
     this.map.addControl(new mapboxgl.AttributionControl({
-      customAttribution: '© Rajdip Ghosh',
+      customAttribution: '<a href="https://rajdipghosh.vercel.app/#contact" target="_blank">© Rajdip Ghosh</a>',
     }))
 
     // Create a new MapboxDirections instance
@@ -52,6 +60,37 @@ export class MapsComponent {
     this.map.addControl(this.directions, 'top-left');
     const switcherControl: any = new MapboxStyleSwitcherControl()
     this.map.addControl(switcherControl);
+
+
+    this.draw = new MapboxDraw({
+      displayControlsDefault: false,
+      // Select which mapbox-gl-draw control buttons to add to the map.
+      controls: {
+        polygon: true,
+        trash: true,
+      },
+      // Set mapbox-gl-draw to draw by default.
+      // The user does not have to click the polygon control button first.
+      defaultMode: 'draw_polygon'
+    });
+
+    const updateArea = (e: any) => {
+      const data = this.draw.getAll();
+      console.log("Calling..", data)
+      if (data.features.length > 0) {
+        const area = turf.area(data);
+        // Restrict the area to 2 decimal points.
+        const rounded_area = Math.round(area * 100) / 100;
+        alert(`Total calculated area : ${rounded_area}`)
+      } else {
+        if (e.type !== 'draw.delete')
+          alert('Click the map to draw a polygon.');
+      }
+    }
+
+    this.map.on('draw.create' as MapEventType, updateArea);
+    this.map.on('draw.delete' as MapEventType, updateArea);
+    this.map.on('draw.update' as MapEventType, updateArea);
 
   }
 
@@ -79,4 +118,17 @@ export class MapsComponent {
 
     }, { enableHighAccuracy: true })
   }
+
+  toggle(clickFrom: 1 | 2) {
+    this.mapMode = clickFrom
+
+    if (this.mapMode === 1) {
+      this.map.removeControl(this.draw)
+      this.map.addControl(this.directions, 'top-left');
+    } else {
+      this.map.removeControl(this.directions)
+      this.map.addControl(this.draw, 'top-left');
+    }
+  }
 }
+
